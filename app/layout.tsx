145cd3da65d9a316194cfa17aec600e98fb55b6f
Alvaro Peña <alvpe26@gmail.com>
2023-12-01 10:20:52 -0500

    Removed UserNav from the layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { UserNav } from '@/components/user-nav';
-import { AlignLeft, Bell, HelpCircle } from 'lucide-react';
-import Link from 'next/link';
-import Search from '@/components/search';
 import { MenubarDemo } from '@/components/menu-bar';
-import Footer from '@/components/footer';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Sabio',
-  description: 'True learning',
+  title: 'Anki',
+  description: 'Learn effectively.',
 }
 
 export default function RootLayout({
