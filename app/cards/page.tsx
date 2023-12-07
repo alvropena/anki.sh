fbc826978b2d7fc1118b0d30bd07aa9c17d02b65
alvropena <me@alvropena.com>
2023-12-07 01:48:35 -0500

    Redesigned flashcards

--- app/cards/page.tsx
+++ app/cards/page.tsx
@@ -17,34 +17,42 @@ const MyComponent: React.FC = () => {
         removeCard(cardId);
     };
 
-    // Rest of your component code...
-
     return (
-        <div>
-            <h2>Flashcards</h2>
+        <div className="max-w-md mx-auto p-4 bg-white rounded shadow-md">
+            <h2 className="text-2xl font-bold mb-4">Flashcards</h2>
             <ul>
                 {cards.map((card) => (
-                    <li key={card.id}>
-                        <div>
+                    <li key={card.id} className="mb-4 p-4 border border-gray-200 rounded">
+                        <div className="mb-2">
                             <strong>ID:</strong> {card.id}
                         </div>
-                        <div>
+                        <div className="mb-2">
                             <strong>Front:</strong> {card.front}
-                            <button onClick={() => updateFrontCard(card.id, 'Updated Front')}>
+                            <button
+                                className="ml-2 px-2 py-1 bg-blue-500 text-white rounded"
+                                onClick={() => updateFrontCard(card.id, 'Updated Front')}
+                            >
                                 Update Front
                             </button>
                         </div>
-                        <div>
+                        <div className="mb-2">
                             <strong>Back:</strong> {card.back}
-                            <button onClick={() => updateBackCard(card.id, 'Updated Back')}>
+                            <button
+                                className="ml-2 px-2 py-1 bg-green-500 text-white rounded"
+                                onClick={() => updateBackCard(card.id, 'Updated Back')}
+                            >
                                 Update Back
                             </button>
                         </div>
-                        <button onClick={() => handleRemoveCard(card.id)}>Remove Card</button>
+                        <button
+                            className="px-2 py-1 bg-red-500 text-white rounded"
+                            onClick={() => handleRemoveCard(card.id)}
+                        >
+                            Remove Card
+                        </button>
                     </li>
                 ))}
             </ul>
-            <button onClick={handleAddCard}>Add Card</button>
         </div>
     );
 };
